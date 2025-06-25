df3a12831bcb27baed4a2e2fc3179491a3e57f40
Chris Partridge <chris@partridge.tech>
2025-06-24 20:01:29 -0400

    Fix test counter

--- short_test_with_retry.sh
+++ short_test_with_retry.sh
@@ -5,7 +5,7 @@ until docker run -e TEST_CI='ON' --rm tweedge/tor-uptime-monitor:test
 do
    sleep 1
    [[ counter -eq $max_retry ]] && echo "Ran out of retries!" && exit 1
-   echo "Trying again. Try #$counter"
    ((counter++))
+   echo "Trying again. Try #$counter"
 done
 exit 0
